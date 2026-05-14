6196afc55a4ec5f8c1bc827a7a24c46d8ccca9fb
Jesus Ruvalcaba <108392862+JesusRuvalcaba@users.noreply.github.com>
2026-05-13 19:25:31 -0700

    Update writeback.v

--- Jesus_verilogCode/src/writeback.v
+++ Jesus_verilogCode/src/writeback.v
@@ -1,10 +1,14 @@
-module writeback (
-    input wire memtoreg,
+`timescale 1ns / 1ps
+
+module writeback(
+    input wire MEM_WB_memtoreg,
     input wire [31:0] read_data,
-    input wire [31:0] alu_result,
-    output wire [31:0] write_data
+    input wire [31:0] mem_alu_result,
+
+    output wire [31:0] mem_wb_write_data
 );
 
-    assign write_data = (memtoreg) ? read_data : alu_result;
+assign mem_wb_write_data =
+    (MEM_WB_memtoreg) ? read_data : mem_alu_result;
 
 endmodule
